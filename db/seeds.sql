3e68a5139a62ab078b89a4f44cd84012ea5a9b2c
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-12-11 19:10:44 -0600

    set up files, connected to mysql database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Krabby Patty', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Yum Yum Burg', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Delish', false)
